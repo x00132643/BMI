01987069e26211c90293f7c8e4f48140f926b041
x00132643 <x00132643@outlook.com>
2018-11-17 11:19:26 +0000

    sans-serif

--- BMICalculator/Program.cs
+++ BMICalculator/Program.cs
@@ -19,7 +19,7 @@ namespace BMICalculator
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseApplicationInsights()
+                //.UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
